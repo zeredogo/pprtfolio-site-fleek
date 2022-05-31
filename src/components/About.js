8a07dc696910a6f687c2259cac1638c01f96535f
zeredogo <zeredogo@gmail.com>
2022-05-31 12:21:56 -0700

    custormizes portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,7 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores  magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+                I'm  Paul Dogo  C.E.O DogoEmpire , Blockchain Developer B.s.g Business Admin.  
             </p>
         </section>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,9 +14,8 @@ const Projects = () => {
                 <div className="projects__card">
                     <h3>Portfolio Site</h3>
                     <img src={portfoliosite} alt="PortFolio page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <p>
+                        MY Portfolio web site i Build with React 
                     </p>
                     <a href="https://github.com/zeredogo" target="_blank" className="button">Code</a>
                 </div>
@@ -24,9 +23,8 @@ const Projects = () => {
                 <div className="projects__card">
                     <h3>Resort</h3>
                     <img src={resort} alt="Resort Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <p> 
+                    This is a React Resort i build from ground
                     </p>
                     <a href="https://github.com/zeredogo/dogoempire-react-resort" target="_blank" className="button">Code</a>
                 </div>
@@ -34,9 +32,8 @@ const Projects = () => {
                 <div className="projects__card">
                     <h3>Dogo Task</h3>
                     <img src={dogotask} alt="Task Tracker Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <p>This is just a simply React Task Tracker 2022 with Hooks and 
+                        React Route V6
                     </p>
                     <a href="https://github.com/zeredogo/reacttask2022" target="_blank" className="button">Code</a>
                 </div>
