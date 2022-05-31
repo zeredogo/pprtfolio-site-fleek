b60630ed02cf83c388608cda6e6556e09101745e
zeredogo <zeredogo@gmail.com>
2022-05-31 13:30:52 -0700

    upadated

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,7 +5,7 @@ const Social = () => {
 
             <a href="zeredogo@gmail.com" className="button">Paul@email.com</a>
             <a href="https://twitter.com/paulsundaydogo" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
+            <a href="linkedin.com/in/paul-dogo-b70910211" className="button">LinkedIn</a>
             <a href="https://github.com/zeredogo" className="button">GitHub</a>
         </div>
     );
